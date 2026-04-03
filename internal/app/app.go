3901e9d5cdadd1726ea72d459807150364036b96
tukaram40k <tukaram40k@gmail.com>
2026-04-03 10:03:59 +0300

    add mode selection based on given flags

--- internal/app/app.go
+++ internal/app/app.go
@@ -7,7 +7,18 @@ import (
 
 func Run() {
 	cfg := cli.GetFlags()
+	
 	fmt.Printf("URL: %s\n", cfg.URL)
 	fmt.Printf("Search Results: %s\n", cfg.Search)
 	fmt.Printf("Help: %t\n", cfg.Help)
+
+	// validate url/search here
+
+	if cfg.Help {
+		cli.PrintHelp()
+	} else if cfg.URL != "" {
+		// call url mode finctions
+	} else if cfg.Search != "" {
+		// call search mode functions
+	}
 }
