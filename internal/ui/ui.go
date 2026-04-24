601b550002ac8c68918aeb9c792e9aab54f3d58e
tukaram40k <tukaram40k@gmail.com>
2026-04-24 10:52:10 +0300

    extract ui helper functions into a separate file

--- internal/ui/ui.go
+++ internal/ui/ui.go
@@ -89,52 +89,6 @@ func Error(format string, a ...any) error {
 	return fmt.Errorf(format, a...)
 }
 
-func ensureMaxLineLength(text string, maxChars int) string {
-	if maxChars <= 0 || text == "" {
-		return text
-	}
-
-	lines := strings.Split(text, "\n")
-	wrapped := make([]string, 0, len(lines))
-
-	for _, line := range lines {
-		runes := []rune(line)
-		if len(runes) <= maxChars {
-			wrapped = append(wrapped, line)
-			continue
-		}
-
-		for start := 0; start < len(runes); start += maxChars {
-			end := start + maxChars
-			if end > len(runes) {
-				end = len(runes)
-			}
-			wrapped = append(wrapped, string(runes[start:end]))
-		}
-	}
-
-	return strings.Join(wrapped, "\n")
-}
-
-func normalizedResponseLines(resp *parser.Response, maxChars int) *parser.Response {
-	if resp == nil {
-		return nil
-	}
-
-	normalized := *resp
-	normalized.StatusLine = ensureMaxLineLength(resp.StatusLine, maxChars)
-	normalized.ContentType = ensureMaxLineLength(resp.ContentType, maxChars)
-
-	normalized.HeaderFields = make([]string, len(resp.HeaderFields))
-	for i, header := range resp.HeaderFields {
-		normalized.HeaderFields[i] = ensureMaxLineLength(header, maxChars)
-	}
-
-	normalized.Body = []byte(ensureMaxLineLength(string(resp.Body), maxChars))
-
-	return &normalized
-}
-
 func formatParsedResponse(resp *parser.Response) string {
 	resp = normalizedResponseLines(resp, maxResponseLineLength)
 
