311bdd56b3d6467d047ad97e4d17c696fb4faf87
tukaram40k <tukaram40k@gmail.com>
2026-04-26 20:22:30 +0300

    extract ui style definitions into a separate file

---
+++ internal/ui/ui_definitions.go
@@ -0,0 +1,68 @@
+package ui
+
+import "charm.land/lipgloss/v2"
+
+const (
+	maxResponseLineLength = 80
+
+	colorTextPrimary    = "#b7d0f8"
+	colorTextSecondary  = "#b7f8f4"
+	colorTextOKBadge    = "#052E16"
+	colorTextErrorBadge = "#450A0A"
+	colorTextHTML       = "#0369A1"
+	colorTextBackground = "#5b435a"
+
+	colorBorderPrimary = "#8921d8"
+	colorBorderOK      = "#15803D"
+	colorBorderError   = "#B91C1C"
+)
+
+var (
+	okBadgeStyle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color(colorTextOKBadge)).
+			Background(lipgloss.Color("#86EFAC")).
+			Padding(0, 1)
+
+	errBadgeStyle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color(colorTextErrorBadge)).
+			Background(lipgloss.Color("#FCA5A5")).
+			Padding(0, 1)
+
+	metaLabelStyle = lipgloss.NewStyle().
+			Bold(true).
+			Foreground(lipgloss.Color(colorTextPrimary))
+
+	metaValueStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color(colorTextPrimary))
+
+	panelStyle = lipgloss.NewStyle().
+			Margin(0, 0, 0, 0).
+			Padding(0, 1)
+
+	tableCellStyle = lipgloss.NewStyle().
+			Padding(0, 1)
+
+	tableOddRowStyle = tableCellStyle.
+				Foreground(lipgloss.Color(colorTextPrimary))
+
+	tableEvenRowStyle = tableCellStyle.
+				Foreground(lipgloss.Color(colorTextSecondary))
+
+	headersBoxStyle = lipgloss.NewStyle().
+			Foreground(lipgloss.Color(colorTextPrimary)).
+			Padding(0, 1)
+
+	headersBlockStyle = lipgloss.NewStyle().
+				Border(lipgloss.RoundedBorder()).
+				BorderForeground(lipgloss.Color(colorBorderPrimary))
+
+	bodyPlaceholderStyle = lipgloss.NewStyle().
+				Italic(true).
+				Foreground(lipgloss.Color(colorTextPrimary))
+
+	bodyBlockStyle = lipgloss.NewStyle().
+			Border(lipgloss.RoundedBorder()).
+			BorderForeground(lipgloss.Color(colorBorderPrimary))
+)
